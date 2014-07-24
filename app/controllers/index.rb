62230184ade19b1379d346e13423c71b2a00b409
Ariel Fogel & Kendall Carey <pair+afogel.kendallcarey@devbootcamp.com>
2014-07-24 16:55:37 -0700

    working create new survey page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,9 +2,15 @@ get '/' do
 	erb :index
 end
 
-get '/survey/new' do
+get '/surveys/new' do
   erb :create_survey
 end
 
+post '/surveys' do
+  Survey.create(params)
+  redirect '/surveys/:id/edit'
+end
 
-
+get '/surveys/:id/edit' do
+  erb :edit_survey
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@ Survey.find(1).questions << Question.new(question: "What is your favorite food?"
 
 Question.find(1).choices << Choice.new(choice: "Red")
 Question.find(1).choices << Choice.new(choice: "Blue")
-Question.find(1).choices << Choice.new(choice: "Green")
 
 Question.find(2).choices << Choice.new(choice: "Japanese")
 Question.find(2).choices << Choice.new(choice: "Italian")
-Question.find(2).choices << Choice.new(choice: "Indian")

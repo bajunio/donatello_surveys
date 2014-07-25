668eb5dc4e0765d7af32fd1e6fe271ffdcb63805
Brian Junio & Steven Leu <pair+bajunio.eksentrysyti@devbootcamp.com>
2014-07-24 18:31:14 -0700

    adds routes for creating and updating a survey

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,16 +1,34 @@
+# Splash Page =================
 get '/' do
 	erb :index
 end
 
+# Create Survey (title) =======
 get '/surveys/new' do
   erb :create_survey
 end
 
-post '/surveys' do
+post '/surveys/new' do
   Survey.create(params)
   redirect '/surveys/:id/edit'
 end
 
+# Edit Survey Questions =======
+
 get '/surveys/:id/edit' do
   erb :edit_survey
+end
+
+post '/surveys/:id/questions' do
+	# params (id, question, answer[0], answer[1])
+	survey = Survey.find(params[:id])
+	survey.questions << Question.new(question: params[:new_question])
+
+	# params[:answer].each do |choice|
+	# 	survey.questions.last.choices << Choice.new(choice: choice)
+	# end
+	survey.questions.last.choices << Choice.new(choice: choice1)
+	survey.questions.last.choices << Choice.new(choice: choice2)
+
+	redirect '/surveys/:id/edit'
 end
